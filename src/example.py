972e53f61d2cb0607dcba110b02097098088d0f5
Rohit R. Mohanty <x23113057@student.ncirl.ie>
2024-03-20 05:51:28 +0000

    Simple calculator program for demonstration

---
+++ src/example.py
@@ -0,0 +1,55 @@
+# This function adds two numbers
+def addition(x, y):
+    return x + y
+
+# This function subtracts two numbers
+def subtraction(x, y):
+    return x - y
+
+# This function multiplies two numbers
+def multiplication(x, y):
+    return x * y
+
+# This function divides two numbers
+def division(x, y):
+    return x / y
+
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+while True:
+    # accept input from the user
+    selection = int(input("Enter operation to compute(1/2/3/4): "))
+    print(type(selection))
+    # check if the input is one of the options
+    if selection in (1, 2, 3, 4):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        if selection == 1:
+            print(num1, "+", num2, "=", addition(num1, num2))
+
+        elif selection == 2:
+            print(num1, "-", num2, "=", subtraction(num1, num2))
+
+        elif selection == 3:
+            print(num1, "*", num2, "=", multiplication(num1, num2))
+
+        elif selection == 4:
+            print(num1, "/", num2, "=", division(num1, num2))
+        
+        # check if user wants another calculation
+        # break the while loop if answer is no
+        next_calculation = input("Calculate again? (Y/N): ")
+        if next_calculation == "no":
+          break
+    else:
+        print("Invalid Input")
