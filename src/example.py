a92c1dd01991a148aabe8badeee7726f74fd8d78
Veritia <funkyvish88@gmail.com>
2024-04-10 22:48:00 +0100

    Add exponential function

--- src/example.py
+++ src/example.py
@@ -2,17 +2,25 @@
 def addition(x, y):
     return x + y
 
+
 # This function subtracts two numbers
 def subtraction(x, y):
     return x - y
+
 
 # This function multiplies two numbers
 def multiplication(x, y):
     return x * y
 
+
 # This function divides two numbers
 def division(x, y):
     return x / y
+
+
+# This function calculates exponential value for two numbers
+def exponent(x, y):
+    return x ** y
 
 
 print("Select operation.")
@@ -20,13 +28,14 @@ print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
+print("5.Exponent")
 
 while True:
     # accept input from the user
-    selection = int(input("Enter operation to compute(1/2/3/4): "))
+    selection = int(input("Enter operation to compute(1/2/3/4/5): "))
     print(type(selection))
     # check if the input is one of the options
-    if selection in (1, 2, 3, 4):
+    if selection in (1, 2, 3, 4, 5):
         try:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
@@ -45,11 +54,14 @@ while True:
 
         elif selection == 4:
             print(num1, "/", num2, "=", division(num1, num2))
-        
+
+        elif selection == 5:
+            print(num1, "**", num2, "=", exponent(num1, num2))
+
         # check if user wants another calculation
         # break the while loop if answer is no
         next_calculation = input("Calculate again? (Y/N): ")
         if next_calculation == "no":
-          break
+            break
     else:
         print("Invalid Input")
