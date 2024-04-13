801d5a0b6493cb0df012ae1b99280bfd21c37898
Anita <anita.kuthencz@gmail.com>
2024-04-13 13:06:40 +0100

    Changes function definition

--- src/example.py
+++ src/example.py
@@ -13,20 +13,26 @@ def multiplication(x, y):
 # This function divides two numbers
 def division(x, y):
     return x / y
+ # This function exponents two numbers
+    def exponentiation(x, y):
+        return x ** y
 
 
 print("Select operation.")
-print("1.Add")
+print("1.Addition")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
+print("5.Exponent")
+
+
 
 while True:
     # accept input from the user
-    selection = int(input("Enter operation to compute(1/2/3/4): "))
+    selection = int(input("Enter operation to compute(1/2/3/4/5): "))
     print(type(selection))
     # check if the input is one of the options
-    if selection in (1, 2, 3, 4):
+    if selection in (1, 2, 3, 4, 5):
         try:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
@@ -35,7 +41,8 @@ while True:
             continue
 
         if selection == 1:
-            print(num1, "+", num2, "=", addition(num1, num2))
+            print(num1, "/", num2, "=", division(num1, num2))
+        
 
         elif selection == 2:
             print(num1, "-", num2, "=", subtraction(num1, num2))
@@ -45,6 +52,16 @@ while True:
 
         elif selection == 4:
             print(num1, "/", num2, "=", division(num1, num2))
+        
+        elif selection == 5:
+            print(num1, "**", num2, "=", exponentiation(num1, num2))
+    
+    
+      
+        
+            
+          
+
         
         # check if user wants another calculation
         # break the while loop if answer is no
