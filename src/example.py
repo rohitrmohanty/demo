6bfd47eb2b5fe17bd233e0866d404873fded4a0f
prusinek4 <prusinek4@gmail.com>
2024-04-13 11:28:16 +0100

    Changes function definition

--- src/example.py
+++ src/example.py
@@ -14,19 +14,23 @@ def multiplication(x, y):
 def division(x, y):
     return x / y
 
+# This function exponentiates two numbers
+def exponentation(x, y):
+    return x ** y
 
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
+print("5.Exponentiate")
 
 while True:
     # accept input from the user
-    selection = int(input("Enter operation to compute(1/2/3/4): "))
+    selection = int(input("Enter operation to compute(1/2/3/4/5): "))
     print(type(selection))
     # check if the input is one of the options
-    if selection in (1, 2, 3, 4):
+    if selection in (1, 2, 3, 4, 5):
         try:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
@@ -45,6 +49,9 @@ while True:
 
         elif selection == 4:
             print(num1, "/", num2, "=", division(num1, num2))
+
+        elif selection == 5:
+            print(num1, "**", num2, "=", exponentation(num1, num2))
         
         # check if user wants another calculation
         # break the while loop if answer is no
